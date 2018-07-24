ede286c412830fd6ba7b4388cd151cdfa90deef4
Grigory Bordyugov <gregbg@gmail.com>
2018-07-24 11:28:02 +0200

    Map[A,B]

--- ConsoleIO.scala
+++ ConsoleIO.scala
@@ -1,10 +1,13 @@
 object ConsoleIO {
 
-  sealed trait ConsoleIO[A]
+  sealed trait ConsoleIO[A] {
+    def map[B](f: A => B): ConsoleIO[B] = Map(this, f)
+  }
 
   case class WriteLine[A](line: String, then: () => ConsoleIO[A]) extends ConsoleIO[A]
   case class ReadLine[A](then: String => ConsoleIO[A]) extends ConsoleIO[A]
   case class EndWith[A](value: A) extends ConsoleIO[A]
+  case class Map[A, B](v: ConsoleIO[B], f: B => A) extends ConsoleIO[A]
 
   val helloWorld: ConsoleIO[Int] = WriteLine("HelloWorld!", ()=>EndWith(3))
 
@@ -18,6 +21,7 @@ object ConsoleIO {
   def interpret[A](program: ConsoleIO[A]): A = program match {
     case WriteLine(line, then) => println(line); interpret(then())
     case ReadLine(then)        => interpret(then(readLine()))
+    case Map(v, f)             => f(interpret(v))
     case EndWith(value)        => value
   }
 }
