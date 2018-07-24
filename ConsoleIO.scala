509f7c6865046ee88e1c993bd8ca478c92b39e0a
Grigory Bordyugov <gregbg@gmail.com>
2018-07-24 10:49:35 +0200

    future-like continuations

--- ConsoleIO.scala
+++ ConsoleIO.scala
@@ -2,8 +2,13 @@ object ConsoleIO {
 
   sealed trait ConsoleIO
 
-  case class WriteLine(line: String, then: ConsoleIO) extends ConsoleIO
+  case class WriteLine(line: String, then: () => ConsoleIO) extends ConsoleIO
   case class End() extends ConsoleIO
 
-  val helloWorld: ConsoleIO = WriteLine("HelloWorld!", End())
+  val helloWorld: ConsoleIO = WriteLine("HelloWorld!", ()=>End())
+
+  def interpret(program: ConsoleIO): Unit = program match {
+    case WriteLine(line, then) => println(line); interpret(then())
+    case End()                 => ()
+  }
 }
