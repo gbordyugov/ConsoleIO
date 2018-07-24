083f295649390f5e91e19a19709171befbb929eb
Grigory Bordyugov <gregbg@gmail.com>
2018-07-24 10:55:02 +0200

    user name program

--- ConsoleIO.scala
+++ ConsoleIO.scala
@@ -8,6 +8,13 @@ object ConsoleIO {
 
   val helloWorld: ConsoleIO = WriteLine("HelloWorld!", ()=>End())
 
+  val userNameProgram: ConsoleIO = WriteLine (
+    "Hello, what is your name?", () =>
+    ReadLine(name =>
+        WriteLine("Hello, " + name + "!", () => End())
+        )
+    )
+
   def interpret(program: ConsoleIO): Unit = program match {
     case WriteLine(line, then) => println(line); interpret(then())
     case ReadLine(then)        => interpret(then(readLine()))
