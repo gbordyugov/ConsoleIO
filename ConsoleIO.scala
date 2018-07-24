a22e9ed5485f0f0527a5e38423ea793452ab6f0b
Grigory Bordyugov <gregbg@gmail.com>
2018-07-24 10:51:47 +0200

    ReadLine class

--- ConsoleIO.scala
+++ ConsoleIO.scala
@@ -3,12 +3,14 @@ object ConsoleIO {
   sealed trait ConsoleIO
 
   case class WriteLine(line: String, then: () => ConsoleIO) extends ConsoleIO
+  case class ReadLine(then: String => ConsoleIO) extends ConsoleIO
   case class End() extends ConsoleIO
 
   val helloWorld: ConsoleIO = WriteLine("HelloWorld!", ()=>End())
 
   def interpret(program: ConsoleIO): Unit = program match {
     case WriteLine(line, then) => println(line); interpret(then())
+    case ReadLine(then)        => interpret(then(readLine()))
     case End()                 => ()
   }
 }
